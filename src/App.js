7c1e83e44432cbe5e8509be9ebb20cb79afae529
Muqtadirat <muqtadir.my@gmail.com>
2023-10-18 20:33:24 +0100

    Tailwind form styling

--- src/App.js
+++ src/App.js
@@ -3,7 +3,14 @@ import Form from "./components/Form";
 import "./App.css";
 
 function CustomButton({ onClick, children }) {
-  return <button onClick={onClick}>{children}</button>;
+  return (
+    <button
+      className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 mb-5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+      onClick={onClick}
+    >
+      {children}
+    </button>
+  );
 }
 
 function App() {
@@ -12,23 +19,31 @@ function App() {
   });
 
   return (
-    <div className="App">
-      <header>
-        <h1>Login To Your Account</h1>
-      </header>
+    <div className="App flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8 ">
+      <div className="container sm:mx-auto sm:w-full sm:max-w-sm">
+        <header>
+          <h1 className="mt-10 mb-7 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+            Login To Your Account
+          </h1>
+        </header>
 
-      <Form />
-      <CustomButton onClick={() => login()}>
-        Sign in with Google 🚀
-      </CustomButton>
-      <GoogleLogin
+        <CustomButton onClick={() => login()}>
+          Sign in with Google 🚀
+        </CustomButton>
+        <p className="block text-sm mb-5 font-medium leading-6 text-gray-900">
+          Or use e-mail address
+        </p>
+        <Form />
+
+        {/*<GoogleLogin
         onSuccess={(credentialResponse) => {
           console.log(credentialResponse);
         }}
         onError={() => {
           console.log(`Login Failed`);
         }}
-      />
+      />*/}
+      </div>
     </div>
   );
 }
--- src/components/Form.js
+++ src/components/Form.js
@@ -2,19 +2,39 @@ import React from "react";
 
 function Form() {
   return (
-    <div className="Form">
-      <form id="login-form">
-        <fieldset>
-          <label for="email">
-            Email
-            <input type="text" name="email" />
-          </label>
-          <br />
-          <label for="password">
-            Password
-            <input type="password" name="password" />
-          </label>
-        </fieldset>
+    <div className="Form mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+      <form id="login-form" className="space-y-6" action="#" method="POST">
+        <label
+          for="email"
+          className="block text-sm font-medium leading-6 text-gray-900"
+        >
+          Email{" "}
+        </label>
+        <input
+          type="text"
+          id="email"
+          name="email"
+          className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+        />
+        <label
+          for="password"
+          className="block text-sm font-medium leading-6 text-gray-900"
+        >
+          Password{" "}
+        </label>
+        <input
+          type="password"
+          name="password"
+          className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+          required
+        />
+        <input type="checkbox" id="rememberMe" value="LogRemember" />
+        <label for="rememberMe">Remember Me</label>
+        <input
+          type="submit"
+          value="Login"
+          className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+        />
       </form>
     </div>
   );
