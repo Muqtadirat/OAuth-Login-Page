1a8f02362a8dcbd9d31c8be3fcda665424805127
Muqtadirat <muqtadir.my@gmail.com>
2023-10-18 17:56:57 +0100

    Syntax correction

--- src/index.js
+++ src/index.js
@@ -1,16 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { GoogleOAuthProvider } from '@react-oauth/google';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { GoogleOAuthProvider } from "@react-oauth/google";
+import "./index.css";
+import App from "./App";
 
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <GoogleOAuthProvider clientId='113316330888-83scbm5t2h5dv4bu17j36du8eb2kd1u8.apps.googleusercontent.com'>
   <React.StrictMode>
-    <App />
+    <GoogleOAuthProvider clientId="113316330888-83scbm5t2h5dv4bu17j36du8eb2kd1u8.apps.googleusercontent.com">
+      <App />
+    </GoogleOAuthProvider>
   </React.StrictMode>
-  </GoogleOAuthProvider>
 );
-
